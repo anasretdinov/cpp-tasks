71774159027b93d307e8a7a0d34f2ec8f249dff8
Amir Nasretdinov <lasker6666@gmail.com>
2025-04-11 23:28:24 +0300

    started work

--- smart_pointers/smart_pointers.h
+++ smart_pointers/smart_pointers.h
@@ -1,8 +1,38 @@
-template <typename>
-class SharedPtr {
+#include <memory>
+#include <allocator>
 
-    // YOUR CODE HERE
+
+template <typename T>
+class SharedPtr {
+private:
+    struct ControlBlock{
+        T ptr = nullptr;
+        size_t spcount = 0;
+        ControlBlock
+    };
+    ControlBlock* cblock = nullptr;
+
+    friend template<typename U, typename... Args>
+    SharedPtr<U> make_shared(Args...&& args);
+
+    template<typemame... Args>
+    SharedPtr(Args...&& args) 
+    : cblock(new ControlBlock(std::forward(args)...)) {
+        
+    }
+public:
+    SharedPtr(const SharedPtr<T>& other) 
+    : cblock(other.cblock) {}
+
+    ~SharedPtr() {
+        (cblock -> spcount)
+    }
 };
+
+template<typename T, typename... Args>
+SharedPtr<T> make_shared(Args...&& args) {
+    return SharedPtr<T>(args);
+}
 
 template <typename>
 class WeakPtr {
